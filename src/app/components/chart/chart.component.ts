fe0c8edb2e3a04cbab57c12aec581711d2885373
Benjamin STRABACH <benjamin.strabach@epsi.fr>
2020-11-19 12:47:06 +0100

    Removed grid on Charts

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartDataSets, ChartColor } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { ChartOptions, ChartType, ChartDataSets, ChartScales } from 'chart.js';
+import { Label, Color,} from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
@@ -12,12 +12,39 @@ export class ChartComponent implements OnInit {
   public barChartOptions: ChartOptions = {
     responsive: true,
     maintainAspectRatio: false,
+    scales: {
+      xAxes: [{
+        gridLines: {
+          display: false
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+          display: false
+        }
+      }],
+    },
   };
+  
   public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = false;
   public barChartPlugins = [];
-  // public barChartColor: ChartColor = 
+  public barChartScales: ChartScales = { 
+    xAxes: [{
+      gridLines: {
+        display: false
+      }
+    }],
+    yAxes: [{
+      gridLines: {
+        display: false
+      }
+    }],
+  }
+  public barChartColors: Color[] = [
+    { backgroundColor: '#A476EF' },
+  ]
 
   public barChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,20 +8,6 @@ import { Label } from 'ng2-charts';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  public barChartOptions: ChartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-  };
-  public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType: ChartType = 'bar';
-  public barChartLegend = false;
-  public barChartPlugins = [];
-
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
-  ];
-
   constructor() { }
 
   ngOnInit(): void {
